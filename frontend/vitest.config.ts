21e6211ea4b0971cca880aea3173c767f9244748
Henrique Farias <a.henriquefarias96@gmail.com>
2026-09-07 02:09:26 -0300

    Config gh-pages

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/taskly-system',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
