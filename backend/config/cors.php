adb00ff4cb967ec9b64235ebc32fc5234cd0e57a
Henrique Farias <a.henriquefarias96@gmail.com>
2026-09-07 10:45:21 -0300

    Fix cors exposed headers and env origin integration

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -1,15 +1,28 @@
 <?php
 
 return [
+
     'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout', 'register'],
+
     'allowed_methods' => ['*'],
-    'allowed_origins' => [
-        'https://henriquef96.github.io',
-        'http://localhost:5173',
-    ],
+
+    'allowed_origins' => array_merge(
+        explode(',', env('CORS_ALLOWED_ORIGINS', '')),
+        [
+            'https://henriquef96.github.io',
+            'http://localhost:5173',
+            'http://127.0.0.1:5173',
+        ]
+    ),
+
     'allowed_origins_patterns' => [],
+
     'allowed_headers' => ['*'],
-    'exposed_headers' => [],
+
+    'exposed_headers' => ['X-XSRF-TOKEN'],
+
     'max_age' => 0,
+
     'supports_credentials' => true,
+
 ];
